ce01bf4de11f2aedff35d05bc2662256e2f36e45
Abdulrahman Al-Ghamdi <XxD7ooom.SSxX@Gmail.Com>
2021-12-19 12:56:50 +0300

    Show guidance information when client hit a wrong endpoint

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express()
 
 app.use(express.json())
 
+app.use('*', (req, res) => {
+    res.send(`<b>Welcome to Saudi Universities Directory Server</b><br>
+    this server containing all the public saudi universities information you need<br><br>
+    for more info and how to use the api, please head to<br>
+    (<a href="https://github.com/Abdulrahman-AlGhamdi/SaudiUniversitiesDirectory-Server" target="_blank">Github Project Page</a>)`)
+})
+
 app.listen(5000, () => {
     console.log('Server is running on port 5000')
 })
